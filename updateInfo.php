baa6d31f36c2200c7e25bc6bc4f7ccdb3254880e
jmlmdf <jmlmdf@mail.missouri.edu>
2014-10-17 16:14:11 -0500

    Updated new website test page
    Ajax and css animation is working. Css animation does not work when ajax
    is called.

---
+++ updateInfo.php
@@ -0,0 +1,20 @@
+<?php
+$infoId = empty($_GET['infoId']) ? 'quote1' : $_GET['infoId'];
+
+switch($infoId) {
+	case 'quote1':
+		$info = 'This website was created and designed by James Landy. It includes the accomplishments of Mizzou students James Landy and Jack Boening.</i>.';
+		break;
+	case 'quote2':
+		$info = 'If you have an android device, you should download Cube Field! It is a easy to learn game about finding the blue sphere in a field of blue cubes within the time limit.';
+		break;
+	case 'quote3':
+		$info = 'If you want a website designed feel free to email me. I can build a website to get you noticed.';
+		break;
+	case 'quote4':
+		$info = 'To reach me with questions, comments, or suggestions feel free to email me at jmlmdf@mail.missouri.edu';
+		break;
+}
+
+print $info;
+?>
