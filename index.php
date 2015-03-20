619fc8bdc9dfe0474f24f9959d3e7c31dd421306
JamesL13 <jmlmdf@mail.missouri.edu>
2015-03-20 14:00:18 -0500

    Init commits to redesigned website

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>The Lost Solstice</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link  href="CSS/bootstrap.min.css" rel="stylesheet" >
+    <link href="CSS/main.css" rel="stylesheet" >
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="JS/bootstrap.min.js"></script>
+    <script src="JS/ajax.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Exo+2:400,800' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" media="screen" href="CSS/style.css">
+
+
+
+  
+
+</head>
+
+<body>
+    <div id="particles-js"></div>
+
+      <script>
+            $(document).ready(function(){
+                 $("#middle").hide().fadeIn(2500);
+            }); 
+
+    </script>
+
+<script src="JS/particles.js"></script>
+<script src="JS/app.js"></script>
+
+
+
+        <div id="middle">
+            The Lost Solstice
+            <div id="links">
+            
+                <li><a href ="https://github.com/JamesL13">Github</a></li>
+                <li>By James Landy</li>
+                <li><a href ="#">Contact</a></li>
+            
+        </div>
+        </div>
+
+
+</body>    
+</html>
