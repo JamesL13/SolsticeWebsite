b6a6abbbdf880c20186d44c89e038440ecd412ee
JamesL13 <jmlmdf@mail.missouri.edu>
2015-03-30 15:43:44 -0500

    Updated Website complete
    mobile is the same, nonmobile looks a lot better

--- updateInfo.php
+++ updateInfo.php
@@ -3,7 +3,7 @@ $infoId = empty($_GET['infoId']) ? 'solstice' : $_GET['infoId'];
 
 switch($infoId) {
 	case 'solstice':
-		$info = 'This website was created and designed by Mizzou Student James Landy. Its purpose is to show off projects I have completed.';
+		$info = 'This  website  was  created  and  designed  by  Mizzou  Student  James  Landy.  Its  purpose  is  to  show  off  projects  I  have  completed.';
 		break;
 	case 'skills':
 		$info = 'My skills include coding in C, Java, HTML5/CSS3, PHP, MYSQL, Postgres, Javascript, Ajax, along with being able to learn new languages quickly. I have a strong drive to always be improving my skills and taking on new challenges.';
@@ -20,8 +20,11 @@ switch($infoId) {
 	case 'maze':
 		$info = 'In a group project in advanced algorithm and design we were given the group project of building a maze solving algorithm that gets the shortest path. The maze solver had to be treated like a robot who only knew where the start was. to solve this we coded an algorithm that visits every node in the maze and builds an queue of the nodes. We then run a BFS on the queue to build the shortest path. This was coded in C.';
 		break;
-	case 'contact':
-		$info = 'To reach me with questions, comments, or suggestions feel free to email me at jmlmdf@mail.missouri.edu';
+	case 'strap':
+		$info = 'At Hack Illinois I worked on the opensource code for Strap which involved using nodejs to add to their opensource libraries. I added a few commands to their library as well as helped modularized their GPS location service.';
+		break;
+	case 'ta':
+		$info = 'My current group project involves making the TA selection process electronic. The form will be online built into a database that will store TA applicants. A system will be built around it to allow teachers to make a preference list of which TAs they want based on their resumes and other teachers comments on the TA applicant and allow the System Admin to ultimately assign TAs to each class and section. This will be built with a mysql database, php using codeignitor as the framework, and html, css, and Javascript for the user interface.';
 		break;
 }
 
